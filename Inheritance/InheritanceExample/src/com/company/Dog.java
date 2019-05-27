69ca7dc9f3e9bacaed919bcc475777715006e8a9
umairarshad <umair6622@gmail.com>
2019-05-27 12:28:49 +0500

    overridding

---
+++ Inheritance/InheritanceExample/src/com/company/Dog.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import com.company.Animal;
+
+public class Dog extends Animal {
+
+    private  int eye;
+    private int legs;
+    private int tail;
+    private  int teeth;
+    private  String coat;
+    public Dog(String name,  int size, int weight, int eye, int legs, int tail, int teeth, String coat) {
+        super(name, 1,1, size, weight);
+        //super is used inside a sub-class method definition to call a method defined in the super class
+
+        this.eye=eye;
+        this.legs=legs;
+        this.tail=tail;
+        this.teeth=teeth;
+        this.coat=coat;
+    }
+
+    public void chew(){
+        System.out.println("Dog.chew() is called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() is called");
+        chew();
+        super.eat();
+    }
+}
--- Inheritance/InheritanceExample/src/com/company/Main.java
+++ Inheritance/InheritanceExample/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Animal animal=new Animal("Animal", 1,1,5,5);
+
+        Dog dog= new Dog("Tommy",3,6,2,4,1,20,"Long Silky");
+        dog.eat();
     }
 }
