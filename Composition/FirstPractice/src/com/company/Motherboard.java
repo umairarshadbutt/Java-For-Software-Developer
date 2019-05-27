6d48fb9a3eb4c83a04a280010713d0db376383fc
umairarshad <umair6622@gmail.com>
2019-05-27 18:59:42 +0500

    Motherboard class created, cunstuctor and getters are generated

---
+++ Composition/FirstPractice/src/com/company/Motherboard.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Motherboard {
+    private String model;
+    private String manufacturer;
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
+    public Motherboard(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getRamSlots() {
+        return ramSlots;
+    }
+
+    public int getCardSlots() {
+        return cardSlots;
+    }
+
+    public String getBios() {
+        return bios;
+    }
+}
