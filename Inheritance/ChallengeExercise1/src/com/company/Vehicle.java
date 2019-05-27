1ae70d3577f5c0bccb2297eff57c2eaac3a28906
umairarshad <umair6622@gmail.com>
2019-05-27 16:29:02 +0500

    Vehicle class defined

--- Inheritance/ChallengeExercise1/src/com/company/Vehicle.java
+++ Inheritance/ChallengeExercise1/src/com/company/Vehicle.java
@@ -5,12 +5,39 @@ public class Vehicle {
     private  String size;
 
     private int currentVelocity;
-    private int currentDirectory;
+    private int currentDirection;
 
     public Vehicle(String name, String size) {
         this.name = name;
         this.size = size;
-        this.currentDirectory=0;
+        this.currentDirection=0;
         this.currentVelocity=0;
+    }
+
+    public  void steering(int direction){
+        this.currentDirection+=direction;
+        System.out.println(("Vehicle.steer(): Steering at "+ currentDirection +"degrees."));
+    }
+
+    public void move (int velocity, int direction){
+        currentVelocity=velocity;
+        currentDirection=direction;
+        System.out.println("Vehicle.move(): moving at " + currentVelocity +"in direction "+ currentDirection);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public int getCurrentVelocity() {
+        return currentVelocity;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
     }
 }
