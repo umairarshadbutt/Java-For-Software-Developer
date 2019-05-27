db1b67bb7e9399e0e5b3bc054ca905621b979943
umairarshad <umair6622@gmail.com>
2019-05-27 11:36:54 +0500

    constructor generated

--- Inheritance/InheritanceExample/src/Animal.java
+++ Inheritance/InheritanceExample/src/Animal.java
@@ -4,4 +4,12 @@ public class Animal {
     private  int body;
     private  int size;
     private  int weight;
+
+    public Animal(String name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
 }
