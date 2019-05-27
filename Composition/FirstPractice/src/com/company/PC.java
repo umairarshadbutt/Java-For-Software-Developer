9afaf836a67f0cc78804fd681cc283b1595ca625
umairarshad <umair6622@gmail.com>
2019-05-27 20:08:29 +0500

    instance of Case, mONITOR, Motherboard created in PC class

---
+++ Composition/FirstPractice/src/com/company/PC.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class PC {
+    private Case theCase;
+    private  Monitor monitor;
+    private  Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}
