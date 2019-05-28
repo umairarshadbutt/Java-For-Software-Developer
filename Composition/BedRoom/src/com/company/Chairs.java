a6ebb2cd8815fe8a9a5102825c266aa306fda16a
umairarshad <umair6622@gmail.com>
2019-05-28 12:05:33 +0500

    chair class created and defined

---
+++ Composition/BedRoom/src/com/company/Chairs.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Chairs {
+    private  int chairCount;
+    private  String color;
+
+    public Chairs(int chairCount, String color) {
+        this.chairCount = chairCount;
+        this.color = color;
+    }
+}
