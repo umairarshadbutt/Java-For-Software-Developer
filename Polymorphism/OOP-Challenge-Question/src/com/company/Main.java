edac121acd63ae0686419380bb882fe7ec430f7b
umairarshad <umair6622@gmail.com>
2019-05-28 20:05:42 +0500

    part a completed

--- Polymorphism/OOP-Challenge-Question/src/com/company/Main.java
+++ Polymorphism/OOP-Challenge-Question/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Hamburger hamburger=new Hamburger("ABC", "Roasted",250,"ABC" );
+        hamburger.addition1("souces",30);
+        hamburger.addition2("souces",30);
+
+       System.out.println("Total Burger Price is: " +hamburger.burgurePrice());
     }
 }
