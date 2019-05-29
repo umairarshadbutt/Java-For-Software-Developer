fbbd496eea534f86c81c08f6eac015928d51798b
umairarshad <umair6622@gmail.com>
2019-05-29 14:17:38 +0500

    arrays introduction

--- Arrays/ArraysExample/src/com/company/Main.java
+++ Arrays/ArraysExample/src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner= new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] myIntegers= getIntegers(5);
+
+//        int[] myIntArray = new int[25];
+//
+//        for (int i=0; i<myIntArray.length;i++){
+//            myIntArray[i] = i*10;
+//        }
+        for (int i=0; i<myIntegers.length; i++){
+            System.out.println("Element "+ i +", value is "+ myIntegers[i]);
+        }
+
+        System.out.println("The average is : "+ getAverage(myIntegers));
+
+    }
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values");
+        int[] values= new int[number];
+        for (int i=0; i<values.length; i++){
+            values[i]= scanner.nextInt();
+        }
+        return values;
+    }
+
+    public static double getAverage(int [] array){
+        int sum=0;
+        for (int i=0; i<array.length ; i++){
+            sum+= array[i];
+        }
+
+        return (double)sum/ (double)array.length;
     }
 }
