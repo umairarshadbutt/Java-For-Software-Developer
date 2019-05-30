de5488682dec68387696da20de466c512b6c9ace
umairarshad <umair6622@gmail.com>
2019-05-30 17:14:12 +0500

    resolved updating issue

--- ArrayList/ChallengeArrayList/src/com/company/Main.java
+++ ArrayList/ChallengeArrayList/src/com/company/Main.java
@@ -121,7 +121,7 @@ public class Main {
         System.out.println("0  - to shutdown\n" +
                 "1  - to print contacts\n" +
                 "2  - to add a new contact\n" +
-                "3  - to update existing an existing contact\n" +
+                "3  - to update an existing contact\n" +
                 "4  - to remove an existing contact\n" +
                 "5  - query if an existing contact exists\n" +
                 "6  - to print a list of available actions.");
--- ArrayList/ChallengeArrayList/src/com/company/MobilePhone.java
+++ ArrayList/ChallengeArrayList/src/com/company/MobilePhone.java
@@ -27,6 +27,9 @@ public class MobilePhone {
         if(foundPosition <0) {
             System.out.println(oldContact.getName() +", was not found.");
             return false;
+        } else if (findContact(newContact.getName()) != -1){
+            System.out.println("Contact with name "+ newContact.getName() + " already exists. Update was not succesfull");
+            return false;
         }
 
         this.myContacts.set(foundPosition, newContact);
