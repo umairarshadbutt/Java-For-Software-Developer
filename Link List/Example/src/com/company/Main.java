675eddfb6c286910654c047dabc22075d5a4340e
umairarshad <umair6622@gmail.com>
2019-05-31 18:26:17 +0500

    add element according to index through List array

--- Link List/Example/src/com/company/Main.java
+++ Link List/Example/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,5 +13,23 @@ public class Main {
 
         System.out.println("Balance of customer "+ customer.getName()+" is "+customer.getBalance());
 
+        ArrayList<Integer> intList= new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(2);
+        intList.add(45);
+
+        for (int i=0; i<intList.size() ; i++){
+           System.out.println(i+ " : "+ intList.get(i));
+        }
+
+        intList.add(1,3);
+
+        for (int i=0; i<intList.size() ; i++){
+            System.out.println(i+ " : "+ intList.get(i));
+        }
+
     }
+
+
 }
