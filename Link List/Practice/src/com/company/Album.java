db20558bf031ee6f991f6e678253b2a77ea2ae62
umairarshad <umair6622@gmail.com>
2019-06-11 13:05:56 +0500

    addSong, findSong, addToPlayList(with trackNumber and with song title) methods are added

--- Link List/Practice/src/com/company/Album.java
+++ Link List/Practice/src/com/company/Album.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Album {
     private String name;
@@ -13,5 +14,42 @@ public class Album {
         this.songs = songs;
     }
 
+    public  boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            this.songs.add(new Song(title, duration));
+                return true;
+        }
+        return false;
+    }
 
+    private Song findSong(String title){
+        for ( Song checkedSong : this.songs){
+            if(checkedSong.getTitle().equals(title)){
+                return  checkedSong;
+            }
+        }
+        return null;
+    }
+
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList){
+        int index= trackNumber -1;
+        if((index>0) && (index <= this.songs.size()) ){
+            playList.add(this.songs.get(index));
+            return true;
+        }
+
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList( String title,LinkedList<Song> playList){
+        Song checkedSong = findSong(title);
+        if(checkedSong != null){
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("This song "+ title+ " is not in this album");
+        return false;
+    }
 }
