253f27facc25156b6835def9c2124986d89ea540
umairarshad <umair6622@gmail.com>
2019-06-11 15:12:19 +0500

    add songs and their duraton in main, and get the playlist seprallty with both ways

--- Link List/Practice/src/com/company/Main.java
+++ Link List/Practice/src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -13,6 +14,32 @@ public class Main {
         album.addSong("Main Naraye Mastana", 8.60);
         album.addSong("Dhondoo gy agr malkoon malkoon", 8.50);
         album.addSong("Ghoom Charakhra", 8.20);
+
+        albums.add(album);
+
+        album=new Album("Asrari" ,"Asrar Shah");
+        album.addSong("Ishq huwa, Barbaad huwa",4.45);
+        album.addSong("Main Zindabad Hun",4.45);
+        album.addSong("kuri",4.45);
+        album.addSong("Skar wanda ray",4.45);
+        album.addSong("Sab ki khair",4.45);
+        album.addSong("Wah Jo Kalam",4.45);
+        albums.add(album);
+
+        LinkedList <Song> playList = new LinkedList<Song>();
+        albums.get(0).addToPlayList("Allah Hik hai",playList);
+        albums.get(0).addToPlayList("Ghoom Charakhra",playList);
+        albums.get(0).addToPlayList("Wah Jo Kalam",playList);
+        albums.get(0).addToPlayList(4,playList);
+
+        albums.get(1).addToPlayList(3,playList);
+        albums.get(1).addToPlayList(5,playList);
+        albums.get(1).addToPlayList(1,playList);
+        albums.get(1).addToPlayList(15,playList);
+
+
+        play(playList);
+
 
     }
 }
