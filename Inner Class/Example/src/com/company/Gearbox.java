4620ef8d6ee89712e778c935d84c11057ac4c516
umairarshad <umair6622@gmail.com>
2019-06-12 20:20:46 +0500

    inner class modifier changed to private

--- Inner Class/Example/src/com/company/Gearbox.java
+++ Inner Class/Example/src/com/company/Gearbox.java
@@ -6,6 +6,7 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private  int maxGears;
     private  int currentNumber =0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -14,13 +15,46 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
-    public class Gear{
+    public void operateClutch(boolean in){
+        this.clutchIsIn=in;
+    }
+
+    public void addGear(int number, double ratio){
+        if ((number > 0) && (number <= maxGears)){
+            this.gears.add(new Gear(number,ratio));
+        }
+    }
+
+    public void changeGear(int newGear){
+        if((newGear > 0) && (newGear <this.gears.size()) && this.clutchIsIn){
+            this.currentNumber = newGear;
+            System.out.println("Gear "+ newGear+ " selected.");
+        } else {
+            System.out.println("Grind!");
+            this.currentNumber=0;
+        }
+    }
+
+    private double wheelSpeed(int revs){
+        if(clutchIsIn){
+            System.out.println("Scream !!!");
+            return 0.0;
+        }
+
+        return revs * gears.get(currentNumber).getRatio();
+    }
+
+    private class Gear{
         public int gearNumber;
         public double ratio;
 
         public Gear(int gearNumber, double ratio) {
             this.gearNumber = gearNumber;
             this.ratio = ratio;
+        }
+
+        public double getRatio() {
+            return ratio;
         }
 
         public double drivespeed(int revs){
