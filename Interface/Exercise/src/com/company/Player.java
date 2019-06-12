fc250e2c2b7e83bab27de5f92cbe1ccded46c036
umairarshad <umair6622@gmail.com>
2019-06-12 14:35:18 +0500

    setter, getter, constructor, implementation method, Overright toString method are inserted

--- Interface/Exercise/src/com/company/Player.java
+++ Interface/Exercise/src/com/company/Player.java
@@ -1,5 +1,69 @@
 package com.company;
 
-public class Player implements ISaveable {
+import java.util.ArrayList;
 
+public class Player implements ISaveable{
+    private String name;
+    private int hitPoints;
+    private int strength;
+    private String weapon;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                ", weapon='" + weapon + '\'' +
+                '}';
+    }
+
+    @Override
+    public ArrayList<String> write() {
+        return null;
+    }
+
+    @Override
+    public void read(ArrayList<String> savedValues) {
+
+    }
 }
