f21f2336c7f5850fc50173137ba959f567a1481d
umairarshad <umair6622@gmail.com>
2019-06-13 18:23:32 +0500

    declared four absract methods next, setNext, previous, SetPrevious

--- Abstract Classes/Practice/src/ListItem.java
+++ Abstract Classes/Practice/src/ListItem.java
@@ -1,3 +1,17 @@
 public abstract class ListItem {
+    protected ListItem rightLink = null;
+    protected ListItem leftLink = null;
+
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+
+    abstract ListItem next();
+    abstract ListItem setNext(ListItem item);
+    abstract ListItem previous();
+    abstract ListItem setPrevious(ListItem item);
+
 
 }
