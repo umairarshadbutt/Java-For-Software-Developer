8cdc8562c741f6d3869dc65bc7af6336d71ed920
umairarshad <umair6622@gmail.com>
2019-06-13 19:09:52 +0500

    MyLinkList class generated and implements methods

---
+++ Abstract Classes/Practice/src/MyLinkList.java
@@ -0,0 +1,28 @@
+public class MyLinkList implements NodeList{
+
+    private ListItem root= null;
+
+    public MyLinkList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return null;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}
