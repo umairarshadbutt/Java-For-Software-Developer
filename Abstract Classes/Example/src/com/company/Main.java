305184791c619a52960e4745869c17bb27681c14
umairarshad <umair6622@gmail.com>
2019-06-13 14:09:20 +0500

    abstract methods of abstract class accessed

--- Abstract Classes/Example/src/com/company/Main.java
+++ Abstract Classes/Example/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Dog dog= new Dog("Tommy");
+        dog.breath();
+        dog.eat();
     }
 }
