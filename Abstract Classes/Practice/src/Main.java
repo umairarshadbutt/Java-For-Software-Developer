010303d781f9f4a066188367ee97df33ed4724c4
umairarshad <umair6622@gmail.com>
2019-06-13 21:24:39 +0500

    addItem and traverse methods are defined

--- Abstract Classes/Practice/src/Main.java
+++ Abstract Classes/Practice/src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MyLinkList list= new MyLinkList(null);
+        list.traverse(list.getRoot());
+
+        String strinData = "Create a string data array to avoid typing loads of addItem instructions";
+
+        String[] data = strinData.split(" ");
+        for(String s: data){
+            list.addItem(new Node(s));
+        }
+
+        list.traverse(list.getRoot());
     }
 }
--- Abstract Classes/Practice/src/MyLinkList.java
+++ Abstract Classes/Practice/src/MyLinkList.java
@@ -8,11 +8,41 @@ public class MyLinkList implements NodeList{
 
     @Override
     public ListItem getRoot() {
-        return null;
+        return this.root;
     }
 
     @Override
-    public boolean addItem(ListItem item) {
+    public boolean addItem(ListItem newItem) {
+        if(this.root == null){
+            this.root = newItem;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if (comparison < 0) {
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    currentItem.setNext(newItem).setPrevious(currentItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem).setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem).setPrevious(newItem);
+
+                } else {
+                    newItem.setNext(this.root).setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            } else {
+                System.out.println(newItem.getValue() +"is already present, not added. ");
+                return false;
+            }
+        }
         return false;
     }
 
@@ -23,6 +53,13 @@ public class MyLinkList implements NodeList{
 
     @Override
     public void traverse(ListItem root) {
-
+        if(root == null) {
+            System.out.println("The list is empty");
+        } else {
+            while (root != null) {
+                System.out.println(root.getValue());
+                root = root.next();
+            }
+        }
     }
 }
