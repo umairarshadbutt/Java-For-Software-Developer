a0f5889f1c5b92880d4747ad57d42951450aa45d
umairarshad <umair6622@gmail.com>
2019-06-13 11:38:23 +0500

    Linked List example moved here

---
+++ Inner Class/Challenge/src/com/company/Song.java
@@ -0,0 +1,22 @@
+package com.company;
+
+
+public class Song {
+    private  String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title+ ": "+ this.duration;
+    }
+}
+
