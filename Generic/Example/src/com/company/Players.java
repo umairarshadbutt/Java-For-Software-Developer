2b9ee73d5e5006abbcc74c471bab457684fbe962
umairarshad <umair6622@gmail.com>
2019-06-14 18:27:54 +0500

    abstract class generated

---
+++ Generic/Example/src/com/company/Players.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public abstract class Players {
+    private String name;
+
+    public Players(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
