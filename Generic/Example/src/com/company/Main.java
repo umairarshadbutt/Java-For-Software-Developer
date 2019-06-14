16d35a5e33dd1937c301c2b8dedfea85ad5785a3
umairarshad <umair6622@gmail.com>
2019-06-14 22:16:06 +0500

    second part completed

--- Generic/Example/src/com/company/Main.java
+++ Generic/Example/src/com/company/Main.java
@@ -9,12 +9,30 @@ public class Main {
         BaseballPlayer ahmad = new BaseballPlayer("Ahmad");
         SoccerPlayer shami = new SoccerPlayer("Shami");
 
-        Team qalandars = new Team("Lahore Qalandars");
+        Team<FootballPlayer> qalandars = new Team("Lahore Qalandars");
         qalandars.addPlayer(tahir);
-        qalandars.addPlayer(shami);
-        qalandars.addPlayer(ahmad);
+//        qalandars.addPlayer(shami);
+//        qalandars.addPlayer(ahmad);
 
 
         System.out.println(qalandars.numPlayers());
+
+        Team<BaseballPlayer> baseballPlayerTeam = new Team<>("Peshawar Zalmi");
+        baseballPlayerTeam.addPlayer(ahmad);
+
+        Team<SoccerPlayer> soccerPlayerTeam = new Team<>("Islamabad");
+        soccerPlayerTeam.addPlayer(shami);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
+        brokenTeam.addPlayer(shami);
+
+        Team<FootballPlayer> teamA= new Team<>("TeamA");
+        Team<FootballPlayer> teamB= new Team<>("TeamB");
+
+        teamA.matchResult(teamB, 1,0);
+        teamB.matchResult(teamA,8,9);
+
+
+
     }
 }
--- Generic/Example/src/com/company/Team.java
+++ Generic/Example/src/com/company/Team.java
@@ -1,15 +1,22 @@
+
+
+
+
+
+
 package com.company;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T extends Players> {
     private String name;
-    int played=0;
+    int played = 0;
     int won = 0;
+    int lost = 0;
     int tied = 0;
-    int lost=0;
 
-    private ArrayList<Players> members= new ArrayList<>();
+
+    private ArrayList<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -19,40 +26,67 @@ public class Team {
         return name;
     }
 
-
-    public boolean addPlayer(Players player){
-        if (members.contains(player)){
+    public boolean addPlayer(T player) {
+        if (members.contains(player)) {
             System.out.println(player.getName() + " is already on this team");
             return false;
         } else {
             members.add(player);
-            System.out.println(player.getName()+ " picked for team "+ this.name);
+            System.out.println(player.getName() + " picked for team " + this.name);
             return true;
         }
     }
 
-    public int numPlayers(){
+    public int numPlayers() {
         return this.members.size();
     }
 
-    public void matchResult(Team opponent, int ourScore, int theirScore){
-        if (ourScore > theirScore){
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+
+        String message;
+
+        if(ourScore > theirScore) {
             won++;
-        } else if(ourScore == theirScore){
+            message = " beat ";
+        } else if(ourScore == theirScore) {
             tied++;
+            message = " drew with ";
+
         } else {
             lost++;
+            message = " lost to ";
+        }
+        played++;
+        if(opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
+            opponent.matchResult(null, theirScore, ourScore);
         }
     }
 
-
-    public int ranking(){
-        return (won*2) + tied;
+    public int ranking() {
+        return (won * 2) + tied;
     }
 
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
