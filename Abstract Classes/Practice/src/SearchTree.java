1235993b4e92728dee26eb3d83b541463f61796f
umairarshad <umair6622@gmail.com>
2019-06-14 17:02:53 +0500

    uncompleted

--- Abstract Classes/Practice/src/SearchTree.java
+++ Abstract Classes/Practice/src/SearchTree.java
@@ -11,7 +11,42 @@ public class SearchTree implements NodeList {
     }
 
     @Override
-    public boolean addItem(ListItem item) {
+    public boolean addItem(ListItem newItem) {
+        if (this.root == null) {
+            // the tree was empty, so our item becomes the head of the tree
+            this.root = newItem;
+            return true;
+        }
+
+        // otherwise, start comparing from the head of the tree
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if (comparison < 0) {
+                // newItem is greater, move right if possible
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    // there's no node to the right, so add at this point
+                    currentItem.setNext(newItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                // newItem is less, move left if possible
+                if (currentItem.previous() != null) {
+                    currentItem = currentItem.previous();
+                } else {
+                    // there's no node to the left, so add at this point
+                    currentItem.setPrevious(newItem);
+                    return true;
+                }
+            } else {
+                // equal, so don't add
+                System.out.println(newItem.getValue() + " is already present");
+                return false;
+            }
+        }
+        // we can't actually get here, but Java complains if there's no return
         return false;
     }
 
