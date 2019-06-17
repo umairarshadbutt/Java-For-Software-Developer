f2d6fcf68e177509a8a21adb8c141713e4ba4124
umairarshad <umair6622@gmail.com>
2019-06-17 12:48:33 +0500

    Method that use a Type paramete as the return Type

--- Generic/GenericMaximumMethod/src/com/company/Main.java
+++ Generic/GenericMaximumMethod/src/com/company/Main.java
@@ -7,4 +7,15 @@ public class Main {
         System.out.printf( "Maximum of %.1f, %.1f and %.1f is %.1f\n\n",6.6, 8.8, 7.7, maximum( 6.6, 8.8, 7.7 ) );
         System.out.printf( "Maximum of %s, %s and %s is %s\n", "pear", "apple", "orange", maximum( "pear", "apple", "orange" ) );
     }
+
+    public static <T extends Comparable<T>> T maximum(T x,T y, T z){
+        T max = x;
+        if(y.compareTo(max) >0 ){
+            max=y;
+        }
+        if (z.compareTo(max)>0){
+            max=z;
+        }
+        return max;
+    }
 }
