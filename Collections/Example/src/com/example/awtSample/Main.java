80147bd8dbee52e78af7c55ed49b224837994d00
umairarshad <umair6622@gmail.com>
2019-06-19 16:48:05 +0500

    collection example added

---
+++ Collections/Example/src/com/example/awtSample/Main.java
@@ -0,0 +1,21 @@
+package com.example.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theartre theartre = new Theartre("Cinipex", 8, 15);
+        theartre.getSeats();
+
+        if(theartre.reserveSeat("H10")){
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if(theartre.reserveSeat("H10")){
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+    }
+}
---
+++ Collections/Example/src/com/example/awtSample/Theartre.java
@@ -0,0 +1,81 @@
+package com.example.awtSample;
+
+import java.util.*;
+
+public class Theartre {
+    private final String theatreName;
+    private List<Seat> seats = new ArrayList<>();
+
+    public Theartre(String theatreName, int numRows, int seatsPerRow) {
+        this.theatreName = theatreName;
+
+        int lastRow = 'A' + (numRows - 1);
+        for (char row = 'A'; row <= lastRow; row++) {
+            for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
+                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                seats.add(seat);
+            }
+        }
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    public boolean reserveSeat(String seatNumber) {
+        Seat requestedSeat = null;
+        for (Seat seat : seats) {
+            if (seat.getSeatNumber().equals(seatNumber)) {
+                requestedSeat = seat;
+                break;
+            }
+        }
+
+        if (requestedSeat == null) {
+            System.out.println("There is no seat " + seatNumber);
+            return false;
+        }
+
+        return requestedSeat.reserve();
+    }
+
+    // for testing
+    public void getSeats() {
+        for (Seat seat : seats) {
+            System.out.println(seat.getSeatNumber());
+        }
+    }
+
+    private class Seat {
+        private final String seatNumber;
+        private boolean reserved = false;
+
+        public Seat(String seatNumber) {
+            this.seatNumber = seatNumber;
+        }
+
+        public boolean reserve() {
+            if (!this.reserved) {
+                this.reserved = true;
+                System.out.println("Seat " + seatNumber + " reserved");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public boolean cancel() {
+            if (this.reserved) {
+                this.reserved = false;
+                System.out.println("Reservation of seat " + seatNumber + " cancelled");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public String getSeatNumber() {
+            return seatNumber;
+        }
+    }
+}
