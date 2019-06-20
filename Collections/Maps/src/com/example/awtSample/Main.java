eebc12dc51b98b8f22dc9293662b77e3ab066c6b
umairarshad <umair6622@gmail.com>
2019-06-20 16:00:01 +0500

    lost project added

--- Collections/Maps/src/com/example/awtSample/Main.java
+++ Collections/Maps/src/com/example/awtSample/Main.java
@@ -1,8 +1,48 @@
 package com.example.awtSample;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+    Theatre theatre = new Theatre("Olympian", 8, 12);
+
+        if(theatre.reserveSeat("D12")) {
+        System.out.println("Please pay for D12");
+    } else {
+        System.out.println("Seat already reserved");
+    }
+
+        if(theatre.reserveSeat("D12")) {
+        System.out.println("Please pay for D12");
+    } else {
+        System.out.println("Seat already reserved");
+    }
+
+        if(theatre.reserveSeat("B13")) {
+        System.out.println("Please pay for B13");
+    } else {
+        System.out.println("Seat already reserved");
+    }
+
+    List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeats);
+    printList(reverseSeats);
+
+
+    List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00", 13.00));
+        priceSeats.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+    printList(priceSeats);
+}
+
+
+    public static void printList(List<Theatre.Seat> list) {
+        for(Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber() + " $" + seat.getPrice());
+        }
+        System.out.println();
+        System.out.println("======================================================================");
     }
 }
