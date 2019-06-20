01f409ea601b20fa18dc18ed999d44c9f709b886
umairarshad <umair6622@gmail.com>
2019-06-20 18:36:27 +0500

    some build in method used according to  map

--- Collections/map/src/com/example/awtSample/maps.java
+++ Collections/map/src/com/example/awtSample/maps.java
@@ -7,24 +7,74 @@ import java.util.Set;
 public class maps {
 
     public static void main(String[] args) {
-        Map<String, String> map= new HashMap<>();
-        map.put("myName", "Umair");
-        map.put("actor", "shaan");
-        map.put("ceo", "Ejaz");
-
-        //System.out.println(map);
-
-        //System.out.println(map.get("actor"));
-
-
-        if (map.containsKey("ceo")){
-            System.out.println("Company will perform excellent");
+        Map<String, String> languages = new HashMap<>();
+        if(languages.containsKey("Java")) {
+            System.out.println("Java already exists");
         } else {
-            System.out.println("It the tough to control company");
+            languages.put("Java", "a compiled high level, object-oriented, platform independent language");
+            System.out.println("Java added successfully");
         }
 
-        for (String key : map.keySet()){
-            System.out.println(key+ " : "+map.get(key));
+        languages.put("Python", "an interpreted, object-oriented, high-level programming language with dynamic semantics");
+        languages.put("Algol", "an algorithmic language");
+        System.out.println(languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code"));
+        System.out.println(languages.put("Lisp", "Therein lies madness"));
+
+        if(languages.containsKey("Java")) {
+            System.out.println("Java is already in the map");
+        } else {
+            languages.put("Java", "this course is about Java");
         }
+
+        System.out.println("================================================");
+
+        languages.remove("Lisp");
+
+        if (languages.remove("Algol", "an algorithmic language")){
+            System.out.println("Algol removed");
+        } else {
+            System.out.println("Algol no removed, key/value pair not found");
+        }
+
+        if (languages.replace("Lisp", "Therein lies madness", "a functional languages")){
+            System.out.println("Lisp replaced");
+        } else {
+            System.out.println("Lisp not replaced");
+        }
+
+
+        for(String key: languages.keySet()) {
+            System.out.println(key + " : " + languages.get(key));
+        }
+
+
+
+
+
+
+
+
+
+
+
+//        Map<String, String> map= new HashMap<>();
+//        map.put("myName", "Umair");
+//        map.put("actor", "shaan");
+//        map.put("ceo", "Ejaz");
+//
+//        //System.out.println(map);
+//
+//        //System.out.println(map.get("actor"));
+//
+//
+//        if (map.containsKey("ceo")){
+//            System.out.println("Company will perform excellent");
+//        } else {
+//            System.out.println("It the tough to control company");
+//        }
+//
+//        for (String key : map.keySet()){
+//            System.out.println(key+ " : "+map.get(key));
+//        }
     }
 }
