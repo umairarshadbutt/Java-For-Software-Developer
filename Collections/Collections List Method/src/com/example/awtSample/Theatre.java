a4d3cb00177a0173a54e5bd3d9be90fbbd9baf8b
umairarshad <umair6622@gmail.com>
2019-06-20 14:29:11 +0500

    Standard declaration of Comparator

--- Collections/Collections List Method/src/com/example/awtSample/Theatre.java
+++ Collections/Collections List Method/src/com/example/awtSample/Theatre.java
@@ -6,18 +6,22 @@ public class Theatre {
     private final String theatreName;
     private List<Seat> seats = new ArrayList<>();
 
-    static final Comparator<Seat> PRICE_ORDER = new Comparator<Seat>() {
-        @Override
-        public int compare(Seat seat1, Seat seat2) {
-            if(seat1.getPrice() <seat2.getPrice()){
-                return -1;
-            } else if(seat1.getPrice() > seat2.getPrice()){
-                return 1;
-            } else {
-                return 0;
+    static final Comparator<Seat> PRICE_ORDER;
+
+    static {
+        PRICE_ORDER = new Comparator<Seat>() {
+            @Override
+            public int compare(Seat seat1, Seat seat2) {
+                if (seat1.getPrice() < seat2.getPrice()) {
+                    return -1;
+                } else if (seat1.getPrice() > seat2.getPrice()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
             }
-        }
-    };
+        };
+    }
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
