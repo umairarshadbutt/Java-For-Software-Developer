40443dab5b596d3037f3f17a08b553af0618c5cc
umairarshad <umair6622@gmail.com>
2019-06-24 18:30:34 +0500

    invalid input issue resolved by exception

--- Exceptional handling/Exception/src/com/example/awtSample/Example.java
+++ Exceptional handling/Exception/src/com/example/awtSample/Example.java
@@ -1,5 +1,6 @@
 package com.example.awtSample;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Example {
@@ -11,13 +12,20 @@ public class Example {
     private static int divide(){
         int x=getInt();
         int y = getInt();
-        System.out.println("x is = "+ x+ ", y = "+ y +"");
+        System.out.println("x is = "+ x+ ", y = "+ y +" 2");
         return x/y;
     }
 
     private static int getInt(){
         Scanner s= new Scanner(System.in);
         System.out.println("Please enter an integer");
-        return s.nextInt();
+        while (true){
+            try {
+                return s.nextInt();
+            } catch (InputMismatchException e){
+                s.nextLine();
+                System.out.println("Please enter a number only the digits 0 to 9 ");
+            }
+        }
     }
 }
