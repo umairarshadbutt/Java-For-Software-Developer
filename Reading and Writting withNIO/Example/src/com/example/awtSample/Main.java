0f61adc3cd483720114f33b1162de45dbe213ebb
umairarshad <umair6622@gmail.com>
2019-06-27 11:34:49 +0500

    write the file

--- Reading and Writting withNIO/Example/src/com/example/awtSample/Main.java
+++ Reading and Writting withNIO/Example/src/com/example/awtSample/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class Main {
@@ -11,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             Path dataPath = FileSystems.getDefault().getPath("data.txt");
+            Files.write(dataPath,"Line 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
             List<String> lines = Files.readAllLines(dataPath);
             for(String line: lines){
                 System.out.println(line);
