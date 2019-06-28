7f692763a43c23ed33d9da2ca6842df378c792b2
umairarshad <umair6622@gmail.com>
2019-06-28 10:59:26 +0500

    copy a file

--- Reading and Writting withNIO/FileSystem/src/com/example/awtSample/Main.java
+++ Reading and Writting withNIO/FileSystem/src/com/example/awtSample/Main.java
@@ -10,32 +10,40 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault().getPath("OutThere.txt");
-        printFile(path);
-        Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
-        printFile(filePath);
-        filePath = Paths.get("/home/azaidi/Desktop/FileSystemText3.txt");
-        printFile(filePath);
-
-        Path path2 = FileSystems.getDefault().getPath("thisFileDoesNotExist.txt");
-        System.out.println(path2.toAbsolutePath());
-
-        filePath = FileSystems.getDefault().getPath("files");
-        System.out.println("Exists = " + Files.exists(filePath));
-
-        filePath = FileSystems.getDefault().getPath("files");
-        System.out.println("Exists = " + Files.exists(path2));
-    }
-
-    private static void printFile(Path path) {
-        try (BufferedReader fileReader = Files.newBufferedReader(path)) {
-            String line;
-            while ((line = fileReader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        try {
+            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1Copy.txt");
+            Files.copy(sourceFile, copyFile);
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
+//        Path path = FileSystems.getDefault().getPath("OutThere.txt");
+//        printFile(path);
+//        Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
+//        printFile(filePath);
+//        filePath = Paths.get("/home/azaidi/Desktop/FileSystemText3.txt");
+//        printFile(filePath);
+//
+//        Path path2 = FileSystems.getDefault().getPath("thisFileDoesNotExist.txt");
+//        System.out.println(path2.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = " + Files.exists(filePath));
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = " + Files.exists(path2));
+//    }
+//
+//    private static void printFile(Path path) {
+//        try (BufferedReader fileReader = Files.newBufferedReader(path)) {
+//            String line;
+//            while ((line = fileReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
 }
