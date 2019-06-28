2a08269755c0e66ee23b16fdbfa5e9145d0661f8
umairarshad <umair6622@gmail.com>
2019-06-28 11:33:37 +0500

    move a file

--- Reading and Writting withNIO/FileSystem/src/com/example/awtSample/Main.java
+++ Reading and Writting withNIO/FileSystem/src/com/example/awtSample/Main.java
@@ -8,13 +8,20 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1Copy.txt");
-            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
 
-            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
-            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
-            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
+            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+            Path destination =FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.move(fileToMove,destination);
+
+//            Copy File and Directory
+//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1Copy1.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e){
             e.printStackTrace();
         }
