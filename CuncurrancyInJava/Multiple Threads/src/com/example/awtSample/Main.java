12abc07a06a42e6f6b02be0962b7b9f2ac22c92e
umairarshad <umair6622@gmail.com>
2019-06-29 14:05:43 +0500

    multiple threads

---
+++ CuncurrancyInJava/Multiple Threads/src/com/example/awtSample/Main.java
@@ -0,0 +1,52 @@
+package com.example.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CountDown countDown= new CountDown();
+
+        CountDownThread t1= new CountDownThread(countDown);
+        t1.setName("Thread 1");
+
+        CountDownThread t2= new CountDownThread(countDown);
+        t1.setName("Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+
+class CountDown {
+    public void doCountDown() {
+        String color;
+        switch (Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ThreadColor.ANSI_CYAN;
+                break;
+            case "Thread 2":
+                color = ThreadColor.ANSI_PURPLE;
+                break;
+            default:
+                color = ThreadColor.ANSI_BLACK;
+        }
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println(color + Thread.currentThread().getName() + ": i = " + i);
+        }
+    }
+}
+
+
+class CountDownThread extends Thread{
+    private CountDown threadCountdown;
+
+    public CountDownThread(CountDown countDown) {
+        threadCountdown = countDown;
+    }
+
+    public void run(){
+
+        threadCountdown.doCountDown();
+    }
+}
