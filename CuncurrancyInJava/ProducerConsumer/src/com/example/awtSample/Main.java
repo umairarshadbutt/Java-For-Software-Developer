b5334f3604c298904df38f1ede85623ca8b4de25
umairarshad <umair6622@gmail.com>
2019-07-01 17:03:28 +0500

    thread pool

--- CuncurrancyInJava/ProducerConsumer/src/com/example/awtSample/Main.java
+++ CuncurrancyInJava/ProducerConsumer/src/com/example/awtSample/Main.java
@@ -3,6 +3,7 @@ package com.example.awtSample;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static com.example.awtSample.Main.EOF;
@@ -16,13 +17,32 @@ public class Main {
         List<String> buffer = new ArrayList<String>();
         ReentrantLock bufferLock = new ReentrantLock();
 
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
         MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW, bufferLock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock);
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future <String> future= executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_RED+"I'm being printed for the callable class");
+                return "This is the callable result";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        } catch (ExecutionException e){
+            System.out.println("Something went wrong");
+        }catch (InterruptedException e){
+            System.out.println("Thread running the task was interrupted");
+        }
+
+        executorService.shutdown();
     }
 }
 
