b25e459b0815ca6bbbea5437f4f93c13ec1e5d41
umairarshad <umair6622@gmail.com>
2019-07-03 18:19:30 +0500

    different uses of replaceAll

--- Regular Expression/Example/src/com/example/awtSample/Main.java
+++ Regular Expression/Example/src/com/example/awtSample/Main.java
@@ -8,5 +8,24 @@ public class Main {
         System.out.println(string);
         String yourString = string.replaceAll("I", "You");
         System.out.println(yourString);
+
+        String alphanumeric = "abcDeeeF12Ghhiiiiiiklo349jjeijdij";
+        System.out.println(alphanumeric.replaceAll(".","Y"));
+
+        System.out.println(alphanumeric.replaceAll("^abcDeee","Y"));
+
+
+        String secondString = "abcDeeeF12GhhiiiabcDeeeiiikloabcDeee349jjeijdij";
+        System.out.println(alphanumeric.replaceAll("^abcDeee","YYY"));
+
+        System.out.println(alphanumeric.matches("^Hello"));
+        System.out.println(alphanumeric.matches("^bcDeee"));
+        System.out.println(alphanumeric.matches("^abcDeeeF12Ghhiiiiiiklo349jjeijdij"));
+
+        System.out.println(alphanumeric.replaceAll("jjeijdij","The Eend"));
+        System.out.println(alphanumeric.replaceAll("[iiiik]","X"));
+        System.out.println(alphanumeric.replaceAll("[iiiik]","I replaced the letter here"));
+        System.out.println(alphanumeric.replaceAll("[klo][jjeijd]","X"));
+
     }
 }
