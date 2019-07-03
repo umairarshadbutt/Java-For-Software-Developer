e8b613357cfe6fad7dc745e7b3ccf1b4e6058fe5
umairarshad <umair6622@gmail.com>
2019-07-03 16:24:17 +0500

    Streams- Flatmap and Lambda Best Practice

--- Streams/src/com/example/awtSample/Main.java
+++ Streams/src/com/example/awtSample/Main.java
@@ -3,6 +3,8 @@ package com.example.awtSample;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -42,6 +44,62 @@ public class Main {
 
         System.out.println("--------------------------");
         System.out.println(concatStream.distinct().peek(System.out::println).count());
+
+
+        Employee john = new Employee("John Doe", 30);
+        Employee jane = new Employee("Jane Deer", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(jane);
+        hr.addEmployee(jack);
+        hr.addEmployee(snow);
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(john);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
+
+        System.out.println("---------------");
+//        List<String> sortedGNumbers = someBingoNumbers
+//                .stream()
+//                .map(String::toUpperCase)
+//                .filter(s -> s.startsWith("G"))
+//                .sorted()
+//                .collect(Collectors.toList());
+
+        List<String> sortedGNumbers = someBingoNumber
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
+        for(String s : sortedGNumbers) {
+            System.out.println(s);
+        }
+        Map<Integer, List<Employee>> groupedByAge = departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .collect(Collectors.groupingBy(employee -> employee.getAge()));
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2)
+                .ifPresent(System.out::println);
+
+        Stream.of("ABC", "AC", "BAA", "CCCC", "XY", "ST")
+                .filter(s -> {
+                    System.out.println(s);
+                    return s.length() == 3;
+                })
+                .count();
 
     }
 }
