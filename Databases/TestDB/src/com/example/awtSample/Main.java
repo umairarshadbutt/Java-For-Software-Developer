9b4972f9f8ecf4997f409de38ad025a61b6efdb2
umairarshad <umair6622@gmail.com>
2019-07-06 12:00:26 +0500

    database created and connect with SQLite browser using JDBC

---
+++ Databases/TestDB/src/com/example/awtSample/Main.java
@@ -0,0 +1,22 @@
+package com.example.awtSample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/home/azaidi/git/jave-practice-question/Databases/TestDB/testJava.db");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+
+            statement.close();
+            conn.close();
+        } catch (SQLException e){
+            System.out.println("Something went Wrong: "+e.getMessage());
+        }
+    }
+}
