8fd5023655e3f93a010b5c0924c586752ba9f2ab
umairarshad <umair6622@gmail.com>
2019-07-06 12:44:08 +0500

    execute the UPDATE query statement

--- Databases/TestDB/src/com/example/awtSample/Main.java
+++ Databases/TestDB/src/com/example/awtSample/Main.java
@@ -1,18 +1,28 @@
 package com.example.awtSample;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
         try{
             Connection conn = DriverManager.getConnection("jdbc:sqlite:/home/azaidi/git/jave-practice-question/Databases/TestDB/testJava.db");
+            //conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS contacts"+" (name TEXT, phone INTEGER, email TEXT)" );
-            statement.execute("INSERT INTO contacts (name, phone, email)" + "VALUES('Umair', 65897542,'umair@email.com')");
+           // statement.execute("INSERT INTO contacts (name, phone, email)" + "VALUES('Qasim', 45565767,'qasim@email.com')");
+
+            statement.execute("UPDATE contacts SET phone=123233 WHERE name='Tim'");
+
+
+//            statement.execute("SELECT * FROM contacts");
+//            ResultSet results = statement.getResultSet();
+//            while(results.next()) {
+//                System.out.println(results.getString("name") + " " +
+//                        results.getInt("phone") + " " +
+//                        results.getString("email"));
+//            }
+
 
             statement.close();
             conn.close();
